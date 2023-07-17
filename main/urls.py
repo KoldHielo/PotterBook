c50156b56bc97d3a22f95df2b15955437f447d4e
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-07-17 01:22:58 +0100

    Removed depreciated url for re_path

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.static import serve
 
@@ -14,8 +13,8 @@ urlpatterns = [
   path('connect/', views.connect, name='connect'),
   path('disconnect', views.disconnect, name='disconnect'),
   path('logout/', views.logout_click, name='logout'),
-  url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-  url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+  re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+  re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
   path('editavailability/', views.edit_availability, name='edit_availability'),
   path('retreiveappointments/<str:slug>/', views.retreive_dates, name='retreive_dates'),
   path('business/<str:slug>/', views.business_schedule, name='business_schedule'),
