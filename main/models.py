06ff8c6d5277436d0d2d0e8b802f4183ef092db3
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-07-24 14:44:01 +0100

    Dynamic deletion of AWS S3 user instance folder

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
+import os
+import shutil
 
 # Create your models here.
 
@@ -25,6 +28,12 @@ class CustomBusinessUser(models.Model):
 
   def __str__(self):
     return self.business_slug
+    
+  def delete(self, *args, **kwargs):
+    folder_path = os.path.join(settings.MEDIA_ROOT, f'businesses/{self.id}')
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+      shutil.rmtree(folder_path)
+    super().delete(*args, **kwargs)
 
 class Service(models.Model):
   business = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
