99e071f7313071da48af0a336e796f3fba65243a
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-07-24 13:32:51 +0100

    Adding EmailToken to admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Appointment, CustomBusinessUser, Service
+from .models import Appointment, CustomBusinessUser, Service, EmailToken
 
 # Register your models here.
 admin.site.register(Appointment)
 admin.site.register(CustomBusinessUser)
 admin.site.register(Service)
+admin.site.register(EmailToken)
