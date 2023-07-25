99d2b3c28f424cfa231ad3285897ea5132425e3a
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-07-25 19:05:03 +0100

    Update urls.py update admin url

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('control-panel-koldhielo-april/', admin.site.urls),
     path('', include('main.urls')),
 ]
