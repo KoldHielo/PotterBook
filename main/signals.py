f907a5e930736373bb58161f22d59ba100dadcbf
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-08-06 13:23:56 +0100

    Update signals.py

--- main/signals.py
+++ main/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from main.models import CustomBusinessUser
+from main.models import CustomBusinessUser, Appointment
 import boto3
+from django.contrib.auth import get_user_model
 
 @receiver(pre_delete, sender=CustomBusinessUser)
 def delete_s3_bucket(sender, instance, **kwargs):
@@ -9,3 +10,11 @@ def delete_s3_bucket(sender, instance, **kwargs):
   bucket = s3.Bucket('potterbook')
   for obj in bucket.objects.filter(Prefix=f'businesses/{instance.id}/'):
     s3.Object(bucket.name, obj.key).delete()
+
+@receiver(pre_delete, sender=get_user_model())
+def delete_user_appointments(sender, instance, **kwargs):
+  appointments = Appointment.objects.filter(
+    business=instance,
+    is_booked=False
+  )
+  appointments.delete()
