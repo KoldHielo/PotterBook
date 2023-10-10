b6d9259f244c3ec80ac5f59483101501637f2e15
Ubuntu <ubuntu@ip-172-31-18-58.eu-north-1.compute.internal>
2023-10-10 23:49:12 +0000

    Configured Nginx for serving of static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'potterbook.co',
     '.potterbook.co',
-    '172.31.18.58'
 ]
 
 # FORM SUBMISSION
@@ -134,9 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ubuntu/webdev/potterbook/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
